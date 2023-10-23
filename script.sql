a31386ba8b28b26df4b7fdcc09ed7d253c322331
dmt-100 <dmt-100@yandex.ru>
2023-10-23 21:22:05 +0300

    v5

---
+++ script.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    username VARCHAR(128) PRIMARY KEY ,
+    firstname VARCHAR(128),
+    lastname VARCHAR(128),
+    birth_date DATE,
+    age INT
+);
--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,5 +1,6 @@
 package com.dmdev;
 
+import com.dmdev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,26 +8,39 @@ import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.concurrent.BlockingDeque;
 
 public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException, InterruptedException {
 //        BlockingDeque<Connection> pool = null;
-//        Connection connection1 = pool.take();
-//
-//        Connection connection = DriverManager
-////                .getConnection("jdbc:postgresql://localhost:5433/hibernate-starter-dmdev", "postgres", "pass");
-//                .getConnection("db.url", "db.username", "db.password");
+//        Connection connection = pool.take();
+//        SessionFactory
 
+//        Connection connection = DriverManager
+//                .getConnection("db.url", "db.username", "db.password");
+//        Session
         Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
-        try(SessionFactory sessionFactory = configuration.buildSessionFactory();
-            Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
-
-
     }
+
 }
